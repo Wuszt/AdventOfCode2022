ae9dfadebf893dd7567418e77db5993fee2bf187
Wuszt <igoh4ni@gmail.com>
2022-12-04 23:15:08 +0100

    One more improvement

--- AOC_4/AOC_4.py
+++ AOC_4/AOC_4.py
@@ -1,4 +1,4 @@
 xs = [line.strip().split(',') for line in open("input.txt", "r")]
 rs = [[[int(y) for y in z.split('-')] for z in x] for x in xs]
-print(sum([(r[0][0]-r[1][0]) * (r[1][1]-r[0][1]) for r in rs]))
-print(sum([(r[1][0]-r[0][1]) * (r[0][0]-r[1][1]) for r in rs]))
+print(sum([(r[0][0]-r[1][0]) * (r[1][1]-r[0][1]) >= 0 for r in rs]))
+print(sum([(r[1][0]-r[0][1]) * (r[0][0]-r[1][1]) >= 0 for r in rs]))
