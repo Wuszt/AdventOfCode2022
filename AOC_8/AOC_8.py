c51c7fe6eab847e33864cfaa8028d088d10846aa
Kuba Kuźnik <igoh4ni@gmail.com>
2022-12-08 16:00:12 +0100

    Made it smaller

--- AOC_8/AOC_8.py
+++ AOC_8/AOC_8.py
@@ -1,16 +1,12 @@
 map = [x.strip() for x in open("input.txt").readlines()]
 
-def ProcessTree(curr, results, x0, x1, y0, y1, it, b = True):
+def PT(curr, results, x0, x1, y0, y1, it, b = True):
     alls = [b:= b & (curr > z) for z in [map[y][x] for y in range(y0,y1,it) for x in range(x0,x1,it)]]
     return results[0] | all(alls), results[1] * (1 + sum([int(x) for x in alls]) - (bool(alls) and alls[-1]))
 
 def ProcessTrees(x,y):
-    result = ProcessTree(map[y][x], (False, 1), x, x+1, y+1, len(map), 1)
-    result = ProcessTree(map[y][x], result, x+1, len(map[0]), y, y+1, 1)
-    result = ProcessTree(map[y][x], result, x, x-1, y-1, -1, -1)
-    result = ProcessTree(map[y][x], result, x-1, -1, y, y-1, -1)
-    return result
+    res = PT(map[y][x], PT(map[y][x],(False, 1), x, x+1, y+1, len(map), 1), x+1, len(map[0]), y, y+1, 1)
+    return PT(map[y][x], PT(map[y][x], res, x-1, -1, y, y-1, -1), x, x-1, y-1, -1, -1)
 
 results = [ProcessTrees(x,y) for y in range(len(map)) for x in range(len(map[0]))]
-print("Part1: " + str(sum([x[0] for x in results])))
-print("Part2: " + str(max([x[1] for x in results])))
+print("Part1: {} | Part2: {}".format(sum([x[0] for x in results]),max([x[1] for x in results])))
