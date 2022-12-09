f62735b707771a82c293a70c9ad045c613088ba7
Kuba Kuźnik <igoh4ni@gmail.com>
2022-12-09 11:27:59 +0100

    Minor improvements

--- AOC_9/AOC_9.py
+++ AOC_9/AOC_9.py
@@ -1,19 +1,15 @@
+dirs = { "L": [-1,0], "R": [1,0], "U": [0,1], "D": [0,-1] }
 cmds = [x.strip().split() for x in open("input.txt").readlines()]
 grid = set()
 KNOTS_AMOUNT = 9
 nodes = [[0,0] for _ in range(0,KNOTS_AMOUNT + 1)]
 for cmd in cmds:
-    dir = [0,0]
-    if cmd[0] == 'L': dir[0] = -1
-    elif cmd[0] == 'R': dir[0] = 1
-    if cmd[0] == 'U': dir[1] = 1
-    elif cmd[0] == 'D': dir[1] = -1
     for i in range(int(cmd[1])):
-        nodes[0] = [nodes[0][0] + dir[0], nodes[0][1] + dir[1]]
+        nodes[0] = [nodes[0][0] + dirs[cmd[0]][0], nodes[0][1] + dirs[cmd[0]][1]]
         for i in range(1,len(nodes)):
             diff = [nodes[i-1][0] - nodes[i][0], nodes[i-1][1] - nodes[i][1]]
             if max(abs(diff[0]), abs(diff[1])) > 1:
                 nodes[i][0] += (diff[0] / max(1,abs(diff[0])))
                 nodes[i][1] += (diff[1] / max(1,abs(diff[1])))        
         grid.add((nodes[-1][0],nodes[-1][1]))
-print(len(grid))
+print("Tail visited {} unique cells".format(len(grid)))
