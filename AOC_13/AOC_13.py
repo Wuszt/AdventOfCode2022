f92c37aeafe96f0b7fc735d60eccc2d6fd5d5a2c
Kuba Kuźnik <igoh4ni@gmail.com>
2022-12-13 11:22:36 +0100

    Day 13

---
+++ AOC_13/AOC_13.py
@@ -0,0 +1,19 @@
+from functools import cmp_to_key
+
+def IsArray(val): return hasattr(val, "__len__")
+def ConvertToArray(val):
+    if IsArray(val): return val
+    else: return [val]
+
+def Check(left, right):
+    if not (IsArray(left) or IsArray(right)): return right - left
+    left, right = ConvertToArray(left), ConvertToArray(right)
+    for i in range(min(len(left), len(right))):
+        outcome = Check(left[i], right[i])
+        if outcome != 0: return outcome
+    return len(right) - len(left)
+
+rows = [eval(x.strip()) for x in open("input.txt").readlines() if len(x.strip()) > 0]
+print(sum([(i/2 +1)* int(Check(rows[i], rows[i+1]) > 0) for i in range(0,len(rows) - 1,2)]))
+rows = sorted(rows + [[[2]],[[6]]], key=cmp_to_key(Check), reverse=True)
+print((rows.index([[2]]) + 1) * (rows.index([[6]]) + 1))
